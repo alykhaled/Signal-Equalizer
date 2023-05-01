c0514d591116a3fcbee7ea29bad626da1ed59380
Aly Khaled <alykhaled2001@live.com>
2023-05-01 10:14:33 +0300

    Clean Code

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const speedSlider         = document.getElementById("speed");
 const speedLabel          = document.getElementById("speed-label");
 const dropdowns           = document.querySelectorAll('[data-dropdown-toggle]');
 const collapses           = document.querySelectorAll('[data-collapse-toggle]');
+const modeMenu            = document.getElementById("modesMenu");
+const modeMenuItems       = modeMenu.querySelectorAll("li");
 const inputCtx            = inputSpectrogram.getContext("2d");
 const outputCtx           = outputSpectrogram.getContext("2d");
 
@@ -19,20 +21,20 @@ inputCtx.fillRect(0, 0, inputSpectrogram.width, inputSpectrogram.height);
 outputCtx.fillStyle = '#000000';
 outputCtx.fillRect(0, 0, outputSpectrogram.width, outputSpectrogram.height);
 
-var isPlaying = false;
-var intervalId = null;
-var updateInterval = 5; // milliseconds
-var currentIndex = 0;
+var isPlaying       = false;
+var intervalId      = null;
+var updateInterval  = 5; // milliseconds
+var currentIndex    = 0;
 
-let audioFile = null;
-let inputData = [];
-let inputTime = [];
-let outputData = [];
-let outputTime = [];
-const maxWidth = 2000;
-const maxFrequency = 1000;
+let audioFile       = null;
+let inputData       = [];
+let inputTime       = [];
+let outputData      = [];
+let outputTime      = [];
+const maxWidth      = 2000;
+const maxFrequency  = 1000;
 
-const inputChart = new Chart(ctx, {
+const inputChart    = new Chart(ctx, {
   type: "line",
   data: {
     labels: [],
@@ -56,39 +58,31 @@ const inputChart = new Chart(ctx, {
   },
 });
 
-const outputChart = new Chart(ctx2, {
-  type: 'line',
+const outputChart   = new Chart(ctx2, {
+  type: "line",
   data: {
     labels: [],
-    datasets: [{
-      label: 'Signal',
-      data: [],
-      borderWidth: 1,
-      radius: 0,
-    }]
+    datasets: [
+      {
+        label: "Signal",
+        data: [],
+        borderWidth: 1,
+        radius: 0,
+      },
+    ],
   },
   options: {
     animation: false,
     scales: {
-      x:
-      {
-          max: maxWidth,
-          min: 0,
+      x: {
+        max: maxWidth,
+        min: 0,
       },
-
-      yAxes: [{
-        ticks: {
-          min: -1,
-          max: 1,
-          stepSize: 0.1
-        }
-      }]
-    }
-  }
+    },
+  },
 });
 
 /////////////////////////////////////////////////////////////////////////////////////
-
 
 class Slider {
   constructor(id, frequencyMin, frequencyMax, value, min, max, step) {
@@ -113,8 +107,62 @@ class Equalizer {
     this.maxWidth = 1500;
     this.maxFrequency = 1000;
   }
-  equalize()
-  {
+  initSliders() {
+    for (var slider of this.sliders) {
+      const sliderMainContainer = document.getElementById("sliders");
+      const sliderContainer = document.createElement("div");
+      sliderContainer.classList.add("slider");
+
+      const sliderElement = document.createElement("input");
+      const sliderLabel = document.createElement("p");
+      sliderLabel.textContent = slider.name;
+
+      const sliderValue = document.createElement("p");
+      sliderValue.textContent = slider.value;
+
+      const sliderMax = document.createElement("p");
+      sliderMax.textContent = slider.max;
+
+      const sliderMin = document.createElement("p");
+      sliderMin.textContent = slider.min;
+
+      sliderElement.classList.add("slide");
+      sliderElement.type = "range";
+      sliderElement.id = slider.id;
+      sliderElement.min = slider.min;
+      sliderElement.max = slider.max;
+      sliderElement.step = slider.step;
+      sliderElement.value = slider.value;
+      sliderElement.addEventListener("input", () => {
+        sliderValue.textContent = sliderElement.value;
+        slider.value = sliderElement.value;
+        this.updateSliders();
+        // this.updateChart();
+      });
+
+      
+      sliderContainer.appendChild(sliderValue);
+      sliderContainer.appendChild(sliderMax);
+      sliderContainer.appendChild(sliderElement);
+      sliderContainer.appendChild(sliderMin);
+      sliderContainer.appendChild(sliderLabel);
+      sliderMainContainer.appendChild(sliderContainer);
+    }
+  }
+
+  updateSliders() {
+    for (const slider of this.sliders) {
+      const sliderElement = document.getElementById(slider.id);
+      slider.value = sliderElement.value;
+    }
+    this.equalize();
+  }
+
+  updateSpectrogram() {
+    drawSpectrogram(outputData,outputSpectrogram);
+  }
+
+  equalize(){
     const gains = [];
     const freqRanges = [];
     for (const slider of this.sliders) {
@@ -137,7 +185,7 @@ class Equalizer {
       this.outputTime = inputTime
       this.outputData = [];
       for (let i = 0; i < data.length; i++) {
-        this.outputData.push(data[i].toFixed(8));
+        this.outputData.push(data[i]);
       }
       outputChart.data.labels = this.outputTime;
       outputChart.data.datasets[0].data = this.outputData;
@@ -150,6 +198,7 @@ class Equalizer {
       
     });
   }
+
   read() {
     const gains = [];
     const freqRanges = [];
@@ -168,7 +217,7 @@ class Equalizer {
       inputData = [];
       for (let i = 0; i < data.length; i++) {
         // inputTime.push(data[i].toFixed(8));
-        inputData.push(data[i].toFixed(8));
+        inputData.push(data[i]);
       }
       inputChart.data.labels = inputTime;
       inputChart.data.datasets[0].data = inputData;
@@ -182,12 +231,42 @@ class Equalizer {
 
 }
 
-class UniformRangeEqulizer extends Equalizer {
+class UniformRangeEqualizer extends Equalizer {
   constructor(inputData, inputTime) {
     super(inputData, inputTime);
     this.addSliders();
     this.initSliders();
 
+  }
+
+  addSliders() {
+    const frequencyStep = this.maxFrequency / 10;
+    for (let i = 0; i < 10; i++) {
+      const slider = new Slider(
+        `uniform-range-slider-${i}`,
+        i*frequencyStep,
+        (i+1)*frequencyStep,
+        1,
+        -20,
+        20,
+        1
+      );
+      // console.log(frequencyStep);
+      this.sliders.push(slider);
+    }
+  }
+
+  equalize() {
+    super.equalize(this.sliders);
+    this.updateSpectrogram();
+  }
+}
+
+class VowelsEqualizer extends Equalizer{
+  constructor(inputData, inputTime) {
+    super(inputData, inputTime);
+    this.addSliders();
+    this.initSliders();
   }
 
   initSliders() {
@@ -235,18 +314,19 @@ class UniformRangeEqulizer extends Equalizer {
   }
 
   addSliders() {
-    const frequencyStep = this.maxFrequency / 10;
-    for (let i = 0; i < 10; i++) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    const minFrequency = [730, 500, 270, 400, 250];
+    const maxFrequency = [1090, 1700, 2290, 800, 595];
+    for (let i = 0; i < vowels.length; i++) {
       const slider = new Slider(
-        `uniform-range-slider-${i}`,
-        i*frequencyStep,
-        (i+1)*frequencyStep,
+        `vowels-slider-${vowels[i]}`,
+        minFrequency[i],
+        maxFrequency[i],
         1,
         -20,
         20,
         1
       );
-      console.log(frequencyStep);
       this.sliders.push(slider);
     }
   }
@@ -274,87 +354,47 @@ class UniformRangeEqulizer extends Equalizer {
   }
 }
 
-class VowelsEqulizer extends Equalizer{
-  constructor() {
-    super();
-    this.sliders = []
-  }
+const uniformRangeEqualizer = new UniformRangeEqualizer(inputData, inputTime);
 
-  initSliders() {
-    for (const slider of this.sliders) {
-      const sliderMainContainer = document.getElementById("sliders");
-      const sliderContainer = document.createElement("div");
-      sliderContainer.classList.add("slider");
 
-      const sliderElement = document.createElement("input");
-      const sliderLabel = document.createElement("p");
-      sliderLabel.textContent = slider.name;
-
-      const sliderValue = document.createElement("p");
-      sliderValue.textContent = slider.value;
-
-      const sliderMax = document.createElement("p");
-      sliderMax.textContent = slider.max;
-
-      const sliderMin = document.createElement("p");
-      sliderMin.textContent = slider.min;
-
-      sliderElement.classList.add("slide");
-      sliderElement.type = "range";
-      sliderElement.id = slider.id;
-      sliderElement.min = slider.min;
-      sliderElement.max = slider.max;
-      sliderElement.step = slider.step;
-      sliderElement.value = slider.value;
-      sliderElement.addEventListener("input", () => {
-        sliderValue.textContent = sliderElement.value;
-      
-        this.updateSliders();
-        this.updateChart();
-      });
-
-      
-      sliderContainer.appendChild(sliderValue);
-      sliderContainer.appendChild(sliderMax);
-      sliderContainer.appendChild(sliderElement);
-      sliderContainer.appendChild(sliderMin);
-      sliderContainer.appendChild(sliderLabel);
-      sliderMainContainer.appendChild(sliderContainer);
+async function drawSpectrogram(data, canvas) {
+  // make post request to server
+  // response is image so take it and view it
+  data = data.map((d) => {
+    // Remove NaN values
+    if (isNaN(d)) {
+      return 0;
     }
-  }
 
-  addSliders() {
-    const vowels = ["a", "e", "i", "o", "u"];
-    const minFrequency = [500, 1000, 1500, 2000, 2500];
-    const maxFrequency = [1000, 1500, 2000, 2500, 3000];
-    for (let i = 0; i < vowels.length; i++) {
-      const slider = new Slider(
-        `vowels-slider-${vowels[i]}`,
-        minFrequency[i],
-        maxFrequency[i],
-        0,
-        -20,
-        20,
-        1
-      );
-      this.sliders.push(slider);
-    }
-  }
+    return d;
+  });
+  const url = "http://127.0.0.1:5000/spectrogram";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: `{"data": [${data}]}`
+  };
+  fetch(url, options)
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(blob) {
+    var url = URL.createObjectURL(blob);
+    var img = new Image();
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      // Add padding to the canvas
+      canvas.height += 20;
+      canvas.getContext('2d').drawImage(img, 0, 0);
 
-  updateSliders() {
-    for (const slider of this.sliders) {
-      const sliderElement = document.getElementById(slider.id);
-      slider.value = sliderElement.value;
-    }
-  }
+    };
+    img.src = url;
+  });
 
-  updateChart() {
-
-  }
 }
-
-const uniformRangeEqulizer = new UniformRangeEqulizer(inputData, inputTime);
-
 
 fileInput.addEventListener('change', async (event) => {
   audioFile = event.target.files[0];
@@ -372,13 +412,14 @@ fileInput.addEventListener('change', async (event) => {
         time.push((i/sampleRate).toFixed(4));
       }
       inputTime = time;
-      uniformRangeEqulizer.read();
-      uniformRangeEqulizer.equalize();
+      uniformRangeEqualizer.read();
+      uniformRangeEqualizer.equalize();
 
     });
   };
   reader.readAsArrayBuffer(audioFile);
 });
+
 
 playButton.addEventListener("click", (e) => {
   if (!isPlaying) {
@@ -431,96 +472,6 @@ speedSlider.addEventListener("input", () => {
  speedLabel.textContent = speedSlider.value;
 });
 
-function map(value, start1, stop1, start2, stop2) {
-  return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-}
-
-async function drawSpectrogram(data, canvas) {
-  // make post request to server
-  // response is image so take it and view it
-  data = data.map((d) => {
-    // Remove NaN values
-    if (isNaN(d)) {
-      return 0;
-    }
-
-    return d;
-  });
-  const url = "http://127.0.0.1:5000/spectrogram";
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: `{"data": [${data}]}`
-  };
-  fetch(url, options)
-  .then(function(response) {
-    return response.blob();
-  })
-  .then(function(blob) {
-    var url = URL.createObjectURL(blob);
-    var img = new Image();
-    img.onload = function() {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      // Add padding to the canvas
-      canvas.height += 20;
-      canvas.getContext('2d').drawImage(img, 0, 0);
-
-    };
-    img.src = url;
-  });
-
-}
-// function drawSpectrogram(data, canvas) {
-//   var width = canvas.width;
-//   var height = canvas.height;
-//   // canvas.width = width;
-//   // canvas.height = height;
-//   var ctx = canvas.getContext("2d");
-//   const gradient = ctx.createLinearGradient(0, 0, 0, height);
-//   gradient.addColorStop(1, "#0000ff");
-//   gradient.addColorStop(0.5, "#00ffff");
-//   gradient.addColorStop(0, "#ffffff");
-//   // fill the canvas with black
-//   ctx.fillStyle = "#000000";
-//   ctx.fillRect(0, 0, width, height);
-
-//   const fftSize = 1024; // FFT size (power of 2)
-//   const binCount = 256; // number of frequency bins (half of FFT size)
-
-//   const fft = new FFT(fftSize, 44100);
-//   // data = data.map((value) => {
-//   //     return map(value, -1, 1, 0, 255);
-//   // });
-//   // Make sure the data array is the same size as the FFT size.
-//   // If it is smaller, then zero-pad it.
-//   data = data.map((value) => {
-//     return value.value;
-//   });
-//   while (data.length < fftSize) {
-//     data.push(0);
-//   }
-//   // if it is larger, then truncate it
-//   if (data.length > fftSize) {
-//       data = data.slice(0, fftSize);
-//   }
-//   fft.forward(data);
-//   const spectrum = fft.spectrum;
-//   const sliceWidth = width / spectrum.length;
-//   let x = 0;
-//   // get max of spectrum data
-//   const maxS = Math.max(...spectrum);
-//   for (let i = 0; i < spectrum.length; i++) {
-//     const h = map(spectrum[i], 0, maxS, 0, height);
-//     ctx.fillStyle = gradient;
-//     ctx.fillRect(x, height - h, sliceWidth, h);
-//     x += sliceWidth;
-//   }
-// }
-
-
 hideSpectrogramBtn.addEventListener("click", (e) => {
   if (hideSpectrogramBtn.checked) {
     inputSpectrogram.classList.remove("hidden");
@@ -544,5 +495,17 @@ collapses.forEach((collapse) => {
     const collapseId = this.getAttribute("data-collapse-toggle");
     const collapse = document.getElementById(collapseId);
     collapse.classList.toggle("hidden");
+  });
+});
+
+
+modeMenuItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    const mode = this.querySelector("a").textContent;
+    const modeButton = document.getElementById("dropdownNavbarLink");
+    const modeLabel = modeButton.querySelector("p");
+    modeLabel.textContent = mode;
+    // uniformRangeEqualizer.mode = mode;
+    uniformRangeEqualizer.equalize();
   });
 });
